497c7384596aca2d674b1fa8e1a63f85470cc86d
nahtanoJP <Jon_p3rron@hotmail.com>
2016-03-10 14:28:54 -0500

    Latest Version
    - Updated Level 1 & 2 Maps
    - Added fading in between scenes

---
+++ NEWHotlineToronto/Assets/Scripts/Instruction/instrction.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class instrction : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject next = null;// click to see the next page
+	public GameObject previous = null;// click to see the previous page
+	public Text pagenumber = null;
+	private Text ui;
+	int CurrentPagenummber= 1;
+	void Start () 
+	{
+		ui = GetComponent<Text> ();
+		pagenumber.GetComponent< Text > ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+		if (Input.GetKeyDown (KeyCode.Q)) 
+		{
+			Application.LoadLevel("MainMenu");	
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			CurrentPagenummber++; 
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			CurrentPagenummber--; 
+		}
+		if (CurrentPagenummber == 1) {
+			ui.text = "Press the  right  and left arrow to move  jumpman right to left on the screen.";
+
+			pagenumber.text = " Pages 1/4";
+
+
+
+		}
+		if (CurrentPagenummber == 2) {
+			ui.text = " Press the up and down arrows to move Jumpman on ladders.";
+
+			pagenumber.text = "Pages 2/4  ";
+		}
+		if (CurrentPagenummber== 3) {
+			ui.text = " Press the space bar to jump. ";
+
+			pagenumber.text = "Pages 3/4  ";
+		}
+		if (CurrentPagenummber == 4) {
+			ui.text = " Avoid  the barrels and fire guys because they will kill you. Use the hamemer to destory your enemies. You can find on them screen. Your goal is to rescue the laday from donky kong ";
+
+			pagenumber.text = " Pages 4/4 ";
+		}
+
+		if (CurrentPagenummber >4) 
+		{
+			CurrentPagenummber= 4;
+		}
+		if (CurrentPagenummber < 1) 
+		{
+			CurrentPagenummber=1;
+		}
+	}
+
+
+
+	}
+
+
+
+
+
---
+++ NEWHotlineToronto/Assets/Scripts/PlayerControl/BulletSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletSpawner: MonoBehaviour {
+	
+	[SerializeField] private GameObject projectilePrefab = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(1)){
+			//Spawn the projectile, setting its position and orientation to that of the spawner game object's transform.
+			GameObject projectile = Instantiate(this.projectilePrefab) as GameObject;
+			projectile.transform.position = this.gameObject.transform.position;
+			projectile.transform.rotation = this.gameObject.transform.rotation;
+		}
+	}
+}
